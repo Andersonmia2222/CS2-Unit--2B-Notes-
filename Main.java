92fb04143a0e2704c7ed92a77126e285890a852d
Andersonmia2222 <mia.anderson2026@gbwl.org>
2025-11-18 16:06:17 +0000

    2.8 notes

--- Main.java
+++ Main.java
@@ -20,6 +20,34 @@ count ++;
 }
 
 
+// for(initialize; test condition; change)
+ for ( int i = 5; i <= 27; i++) {
+System.out.println(i);
+
+ }
+// modify the change step to skip certain numbers 
+
+for (int i = 0; i<= 10; i+=2) {
+
+   System.out.println(i); 
+}
+// watch intial values and operator 
+
+for (int i = 0; i<11; i+=2 ) {
+   System.out.println(i);
+}
+// this loop did the same thing as the loop above it!
+// decrementing for loops are just like counting backwards
+for (int i= 10; i> 0; i--  ) {
+
+   System.out.println(i);
+// print a special message when i is 1 
+
+if (i == 1) {
+
+   System.out.println("Blast off");
+}
+}
 
    } // end main method
 } // end class
